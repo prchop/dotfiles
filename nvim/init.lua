3daaa85c62f06465873f5f7e52dec32b24ca278e
prchop <pramudyo.prakoso@gmail.com>
2024-10-29 19:01:23 +0700

    update: Delete nvim directory

--- nvim/init.lua
+++
@@ -1,4 +0,0 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-require 'nvim-treesitter.install'.compilers = { 'zig' }
-require 'nvim-treesitter.install'.prefer_git = false
