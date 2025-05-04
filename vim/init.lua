5acd2c940c3cb50ebb5a33b1ef0271674f160034
prchop <182120329+prchop@users.noreply.github.com>
2025-05-04 14:09:27 +0700

    save
    save a changes in init.lua

--- vim/init.lua
+++ vim/init.lua
@@ -25,19 +25,19 @@ vim.cmd("highlight Cursor guibg=#928374 guifg=NONE")
 --vim.opt.statusline = "%f %m %r %= %y %l:%c %p%%"
 
 -- local screenkey_available = vim.fn.has("nvim-0.8") == 1
-local is_nvim_compatible = vim.fn.has("nvim") == 1 and vim.version().major >= 0 and vim.version().minor >= 8
-local screenkey_available = is_nvim_compatible
-	and vim.fn.getenv("NVIM_SCREENKEY") ~= nil
-	and pcall(require, "screenkey")
-
-if screenkey_available then
-	vim.g.screenkey_statusline_component = true
-	vim.o.winbar = "%= %{%v:lua.require('screenkey').get_keys()%} %="
-	vim.api.nvim_set_keymap("n", "<leader>sc", ":Screenkey<CR>", { noremap = true, silent = true })
-	require("screenkey").setup({
-		win_opts = {
-			width = 100,
-		},
-	})
-end
+--local is_nvim_compatible = vim.fn.has("nvim") == 1 and vim.version().major >= 0 and vim.version().minor >= 8
+--local screenkey_available = is_nvim_compatible
+--	and vim.fn.getenv("NVIM_SCREENKEY") ~= nil
+--	and pcall(require, "screenkey")
+--
+--if screenkey_available then
+--	vim.g.screenkey_statusline_component = true
+--	vim.o.winbar = "%= %{%v:lua.require('screenkey').get_keys()%} %="
+--	vim.api.nvim_set_keymap("n", "<leader>sc", ":Screenkey<CR>", { noremap = true, silent = true })
+--	require("screenkey").setup({
+--		win_opts = {
+--			width = 100,
+--		},
+--	})
+--end
 
