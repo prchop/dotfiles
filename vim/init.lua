bb3888dce20d373648a9cbfd1d155ec1868f7056
prchop <182120329+prchop@users.noreply.github.com>
2025-05-14 23:59:31 +0700

    add plugins
    coc-settings.json, gitsigns.lua, harpoon.lua

--- vim/init.lua
+++ vim/init.lua
@@ -3,6 +3,8 @@ vim.opt.runtimepath:append(vim.env.HOME .. "/.vim/after")
 vim.opt.packpath = vim.opt.runtimepath:get()
 
 vim.cmd("source ~/.vimrc")
+require 'plugins.gitsigns' -- adds gitsigns recommend keymaps
+require 'plugins.harpoon' -- require harpoon and setup
 
 vim.cmd([[
   highlight Normal guibg=NONE
@@ -19,18 +21,18 @@ vim.cmd([[
 --vim.fn.sign_define("DiagnosticSignHint", { text = "💡", texthl = "DiagnosticHint" })
 
 vim.diagnostic.config({
-  signs = {
-    text = {
-      [vim.diagnostic.severity.ERROR] = "🔥",
-      [vim.diagnostic.severity.WARN] = "❗️",
-      [vim.diagnostic.severity.INFO] = "✨",
-      [vim.diagnostic.severity.HINT] = "💡",
-    },
-  },
-  virtual_text = true,
-  underline = true,
-  update_in_insert = false,
-  severity_sort = true,
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "🔥",
+			[vim.diagnostic.severity.WARN] = "❗️",
+			[vim.diagnostic.severity.INFO] = "✨",
+			[vim.diagnostic.severity.HINT] = "💡",
+		},
+	},
+	virtual_text = true,
+	underline = true,
+	update_in_insert = false,
+	severity_sort = true,
 })
 
 -- Set cursor highlight with background color
@@ -40,34 +42,16 @@ vim.cmd("highlight Cursor guibg=#928374 guifg=NONE")
 --vim.opt.statusline = "%f %m %r %= %y %l:%c %p%%"
 
 local screenkey_available = vim.fn.has("nvim-0.8") == 1
-  and vim.fn.getenv("NVIM_SCREENKEY") ~= nil
-  and pcall(require, "screenkey")
+	and vim.fn.getenv("NVIM_SCREENKEY") ~= nil
+	and pcall(require, "screenkey")
 
 if screenkey_available then
-  vim.g.screenkey_statusline_component = 1
-  vim.o.winbar = "%{%v:lua.require('screenkey').get_keys()%}"
-  vim.api.nvim_set_keymap("n", "<leader>sc", ":Screenkey<CR>", { noremap = true, silent = true })
-  require("screenkey").setup({
-    win_opts = {
-      width = 90,
-    },
-  })
-end
-
--- require harpoon and setup
-local harpoon = require("harpoon")
-harpoon:setup()
-
-vim.keymap.set("n", "<leader>ha", function()
-  harpoon:list():add()
-end)
-vim.keymap.set("n", "<leader>hl", function()
-  harpoon.ui:toggle_quick_menu(harpoon:list())
-end)
-
--- Set <leader>1..<leader>5 to navigate to specific files in harpoon list
-for i = 1, 5 do
-  vim.keymap.set("n", string.format("<leader>%s", i), function()
-    harpoon:list():select(i)
-  end)
+	vim.g.screenkey_statusline_component = 1
+	vim.o.winbar = "%{%v:lua.require('screenkey').get_keys()%}"
+	vim.api.nvim_set_keymap("n", "<leader>sc", ":Screenkey<CR>", { noremap = true, silent = true })
+	require("screenkey").setup({
+		win_opts = {
+			width = 90,
+		},
+	})
 end
---
+++ vim/lua/plugins/harpoon.lua
@@ -0,0 +1,16 @@
+local harpoon = require("harpoon")
+harpoon:setup()
+
+vim.keymap.set("n", "<leader>ha", function()
+	harpoon:list():add()
+end)
+vim.keymap.set("n", "<leader>hl", function()
+	harpoon.ui:toggle_quick_menu(harpoon:list())
+end)
+
+-- Set <leader>1..<leader>6 to navigate to specific files in harpoon list
+for _, idx in ipairs({ 1, 2, 3, 4, 5, 6 }) do
+	vim.keymap.set("n", string.format("<leader>%d", idx), function()
+		harpoon:list():select(idx)
+	end)
+end
